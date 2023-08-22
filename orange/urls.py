55f615a6524832cd7a41efe234bf892cc67c9fb5
STAHI Omar OBS/OINIS <sphy0581@orange.com>
2023-08-22 17:44:47 +0200

    set vnf fields with a condition and need to complete def vnfconfig

--- orange/urls.py
+++ orange/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.test, name="test"),
+    path("", views.vnfconfig, name="vnfconfig")
 ]
 
--- orange/views.py
+++ orange/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import json, os
+import json
 
 def test(request):
     with open("orange/static/Fichier_Conf_1.json","r") as file:
@@ -11,21 +11,16 @@ def test(request):
         nsdversion = request.POST.get('nsdVersion')
         nsddesc = request.POST.get('nsdDescription')
         softwaremin = request.POST.get('softwareMin')
-        hardware = request.POST.get('hardware')
-        id = request.POST.get('generalID')
-        desc = request.POST.get('generalDescription')
-        pmd_cpu_mask = request.POST.get('openvswitchPmdCpuMask')
+        
         with open("orange/static/Fichier_Conf_1.json","w") as file:
             data['version'] = version
             data['nsd']['properties']['name'] = name
             data['nsd']['version'] = nsdversion
             data['nsd']['properties']['description'] = nsddesc
             data['nsd']['properties']['software_min'] = softwaremin
-            data['nsd']['properties']['hardware'].append(hardware)
-            data['general']['id']['value'] = id
-            data['general']['description']['value'] = desc
-            data['general']['open-vswitch']['pmd-cpu-mask']['value'] = pmd_cpu_mask
+            data['general']['id']['value'] = name+"_3int"
             json.dump(data, file, indent=4)
+            
         response = HttpResponse(content_type="application/octet-stream")
         response['Content-Disposition'] = 'attachment; filename="result.json"'
         with open("orange/static/Fichier_Conf_1.json", "rb") as f:
@@ -34,3 +29,45 @@ def test(request):
     return render(request,"test.html")
 
 
+def vnfconfig(request):
+    #test to know witch file we will open
+    i=0
+    with open("orange/static/Fichier_Conf_1.json","r") as file:
+        data = json.load(file)
+    if request.method == 'POST':
+        vnfname = request.POST.get('name${i}')
+        nsdversion = request.POST.get('nsdVersion')
+        nsddesc = request.POST.get('nsdDescription')
+        softwaremin = request.POST.get('softwareMin')
+        
+        with open("orange/static/Fichier_Conf_1.json","w") as file:
+            data['objects'][i]['name']['value'] = vnfname
+            data['nsd']['version'] = nsdversion
+            data['nsd']['properties']['description'] = nsddesc
+            data['nsd']['properties']['software_min'] = softwaremin
+            data['general']['id']['value'] = name+"_3int"
+    return render(request,"test.html")
+
+'''
+**********objects[type==vnf].name.value ==> VNF Name
+
+**********objects[type==vnf].persistence-id.value ==> <VNF_Name>_3int
+
+**********objects[type==vnf].memory.value ==> VNF memory (en GiB)
+
+**********objects[type==vnf].nof-vcpus.value ==> VNF CPUs
+
+objects[type==vnf].cpu-pinning ===> à poser la questionb à Imane
+
+objects[type==vnf].customization.pathnames.items[i].location ===> Cloud-init path i+1
+
+objects[type==vnf].customization.pathnames.items[i].template-content ===> Cloud-init content i+1 (base64)
+
+objects[type==vnf].disks.items[0].location.value ===> "https://10.253.56.133/<VNF File>"
+
+
+ "location": {
+                #file#            "value": "OneOS-vCPE-x86_pi1-6.6.1m2.qcow2"
+            },
+                       
+'''
