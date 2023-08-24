e8aac35fc8c32c56a0e36a83c36d4aef98e5e15b
STAHI Omar OBS/OINIS <sphy0581@orange.com>
2023-08-24 15:02:17 +0200

    fix adding items problem and the ability to have more than one path and content

--- orange/views.py
+++ orange/views.py
@@ -30,9 +30,11 @@ def test(request):
             data = json.load(file)
             i = 0
             inp = 0
-
+            test = 0
+            vnf_cloud_index = 0
             num = int(num)
             objects = data['objects']
+            
             for object in objects:
                 if data['objects'][i]['type']['value'] == "vnf" and num > 0:
                     vnfname = request.POST.get('name'+str(inp))
@@ -53,21 +55,43 @@ def test(request):
                     cloud_init_paths = []
                     cloud_init_contents = []
                     for c in range(4):
-                        #Cloud-init-content1-${i}
-                        #Cloud-init-path1-0
-                        path = request.POST.get(f'Cloud-init-path{c+1}')
-                        content = request.POST.get(f'Cloud-init-content{c+1}')
-                        cloud_init_paths.append(path)
-                        cloud_init_contents.append(content)
-                        data['objects'][i]['customization']['pathnames']['items'][0]['location']['value'] = path
-                        data['objects'][i]['customization']['pathnames']['items'][0]['template-content']['value'] = content
-                    
-                    
+                        path = request.POST.get(f'Cloud-init-path{c+1}{vnf_cloud_index}')
+                        content = request.POST.get(f'Cloud-init-content{c+1}{vnf_cloud_index}')
+                        if path != "" and content != "":
+                            cloud_init_paths.append(path)
+                            cloud_init_contents.append(content)
+                            if test == 0:    
+                                data['objects'][i]['customization']['pathnames']['items'][0]['location']['value'] = path
+                                data['objects'][i]['customization']['pathnames']['items'][0]['template-content']['value'] = content
+                                test+=1
+                            else:
+                                #this way don't work because item point on items[0] so any change in item will be in items[0]
+                                #item = data['objects'][i]['customization']['pathnames']['items'][0]
+
+                                #so i used this approach
+                                item={
+                                    "location": {
+                                        "value": "\/openstack\/latest\/user_data"
+                                    },
+                                    "template-definition": {
+                                        "value": "base64_encoded_content"
+                                    },
+                                    "template-content": {
+                                        "value": "CiNWTSBjb25maWcgZmlsZQpjb25maWcgdGVybWluYWwKY3J5cHRvIGtleSBnZW5lcmF0ZSBkc2EgMjA0OCBuby1jb25maXJtCmlwIHNzaCBlbmFibGUKYmluZCBzc2ggZ2lnYWJpdGV0aGVybmV0IDAvMC4zOTk4CmlwIHNzaCBhdXRoLW1ldGhvZCBhdXRvbWF0aWMKaXAgc3NoIGF1dGgtcmV0cmllcyAzCmlwIHNzaCBhdXRoLXRpbWVvdXQgMzAKaXAgc3NoIHRpbWVvdXQgNjAwCmhvc3RuYW1lICJla2kyIgppbnRlcmZhY2UgZ2lnYWJpdGV0aGVybmV0IDAvMC4zOTk4CiBlbmNhcHN1bGF0aW9uIGRvdDFxIDM5OTgKIGlwIGFkZHJlc3MgMTMuMTMuMTMuMiAyNTUuMjU1LjI1NS4wCmV4aXQKaXAgcm91dGUgMC4wLjAuMCAwLjAuMC4wIDEzLjEzLjEzLjQKCg=="
+                                    }
+                                }
+                                data['objects'][i]['customization']['pathnames']['items'].append(item)
+                                data['objects'][i]['customization']['pathnames']['items'][test]['location']['value'] = path
+                                data['objects'][i]['customization']['pathnames']['items'][test]['template-content']['value'] = content
+                                test+=1
 
                     i+=1
+                    vnf_cloud_index+=1
                     num-=1
                     inp = int(inp)
                     inp+=1
+                    #reinitialize test to zero to avoid index out of range
+                    test = 0
                 elif data['objects'][i]['type']['value'] != "vnf" and num > 0:
                     i+=1
                 elif num == 0:
@@ -91,68 +115,3 @@ def test(request):
         return response
     
     return render(request, "test.html")
-
-
-def vnfconfig(request):
-    #test to know witch file we will open
-    i=0
-    with open("orange/static/Fichier_Conf_1.json","r") as file:
-        data = json.load(file)
-    if request.method == 'POST':
-        vnfname = request.POST.get('name${i}')
-        nsdversion = request.POST.get('nsdVersion')
-        nsddesc = request.POST.get('nsdDescription')
-        softwaremin = request.POST.get('softwareMin')
-        
-        with open("orange/static/Fichier_Conf_1.json","w") as file:
-            data['objects'][i]['name']['value'] = vnfname
-            data['nsd']['version'] = nsdversion
-            data['nsd']['properties']['description'] = nsddesc
-            data['nsd']['properties']['software_min'] = softwaremin
-    return render(request,"test.html")
-
-'''
-**********objects[type==vnf].name.value ==> VNF Name
-
-**********objects[type==vnf].persistence-id.value ==> <VNF_Name>_3int
-
-**********objects[type==vnf].memory.value ==> VNF memory (en GiB)
-
-**********objects[type==vnf].nof-vcpus.value ==> VNF CPUs
-
-**********objects[type==vnf].disks.items[0].location.value ===> "https://10.253.56.133/<VNF File>"
-
-objects[type==vnf].cpu-pinning ===> à poser la questionb à Imane
-
-objects[type==vnf].customization.pathnames.items[i].location ===> Cloud-init path i+1
-
-objects[type==vnf].customization.pathnames.items[i].template-content ===> Cloud-init content i+1 (base64)             
-'''
-
-
-
-
-
-
-'''
-            "customization": {
-                "pathnames": {
-                    "items": [
-                        {
-                            "location": {
-                                "value": "\/openstack\/latest\/user_data"
-                            },
-                            "template-definition": {
-                                "value": "base64_encoded_content"
-                            },
-                            "template-content": {
-                                "value": "CiNWTSBjb25maWcgZmlsZQpjb25maWcgdGVybWluYWwKY3J5cHRvIGtleSBnZW5lcmF0ZSBkc2EgMjA0OCBuby1jb25maXJtCmlwIHNzaCBlbmFibGUKYmluZCBzc2ggZ2lnYWJpdGV0aGVybmV0IDAvMC4zOTk4CmlwIHNzaCBhdXRoLW1ldGhvZCBhdXRvbWF0aWMKaXAgc3NoIGF1dGgtcmV0cmllcyAzCmlwIHNzaCBhdXRoLXRpbWVvdXQgMzAKaXAgc3NoIHRpbWVvdXQgNjAwCmhvc3RuYW1lICJla2kyIgppbnRlcmZhY2UgZ2lnYWJpdGV0aGVybmV0IDAvMC4zOTk4CiBlbmNhcHN1bGF0aW9uIGRvdDFxIDM5OTgKIGlwIGFkZHJlc3MgMTMuMTMuMTMuMiAyNTUuMjU1LjI1NS4wCmV4aXQKaXAgcm91dGUgMC4wLjAuMCAwLjAuMC4wIDEzLjEzLjEzLjQKCg=="
-                            }
-                        }
-                    ]
-                },
-                if items in not none and contents is not none:
-                    data['customization']['pathnames']['items'][i]['location].append(path)
-                    data['customization']['pathnames']['items'][i]['template-content'].append(content)
-                    data['customization']['pathnames']['items'][i] = "base64_encoded_content"
-'''
